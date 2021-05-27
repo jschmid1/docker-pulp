4a4e8af029636817ee09646d8f83c35ebcb3626e
Colin Hutchinson <hutchic@users.noreply.github.com>
2021-05-27 14:09:32 -0400

    fix(statsd): add the ability to set statsd arguments via docker args (#86)

--- pulp-content/entrypoint.sh
+++ pulp-content/entrypoint.sh
@@ -7,7 +7,15 @@ echo "[INFO] Collecting static files"
 pulpcore-manager collectstatic --noinput
 
 echo "[INFO] Starting content server"
+
+declare -a args=()
+for arg in "$@"
+do
+  eval "args[${#args[*]}]=$arg"
+done
+
 exec gunicorn pulpcore.content:server \
+              "${args[@]}" \
               --bind "0.0.0.0:${PULP_CONTENT_BIND_PORT}" \
               --worker-class 'aiohttp.GunicornWebWorker' \
               -w "${PULP_CONTENT_WORKERS}" \
