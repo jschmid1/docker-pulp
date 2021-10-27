97573fdc6ae063e6ec28537a0927fb9beb446e6a
Joshua Schmid <jaiks@posteo.de>
2021-10-27 09:35:06 +0200

    fix(deb): add signing service
    Signed-off-by: Joshua Schmid <jaiks@posteo.de>

--- pulp-core/signing.py
+++ pulp-core/signing.py
@@ -1,21 +1,38 @@
 from pulpcore.app.models.content import AsciiArmoredDetachedSigningService
+from pulp_deb.app.models import AptReleaseSigningService
 
 NAME = "sign-metadata-v1"
+script_path = "/opt/pulp/bin/sign-metadata"
 
 # read an already exported public key
 with open("/tmp/public.key") as key:
     with open("/tmp/public.fpr") as fpr:
         fingerprint = fpr.read().rstrip("\n")
+        pub_key = key.read()
         service_name = f"{NAME}-{fingerprint}"
+        service_name_deb = f"{NAME}-{fingerprint}-deb"
         try:
             AsciiArmoredDetachedSigningService.objects.get(name=service_name)
         except AsciiArmoredDetachedSigningService.DoesNotExist:
-            print(f"Registring Signing service {service_name}")
+            print(f"Registering Signing service {service_name}")
             AsciiArmoredDetachedSigningService.objects.create(
                 name=service_name,
-                public_key=key.read(),
+                public_key=pub_key,
                 pubkey_fingerprint=fingerprint,
-                script="/opt/pulp/bin/sign-metadata",
+                script=script_path,
             )
         else:
             print(f"Signing service {service_name} already present")
+
+        try:
+            AptReleaseSigningService.objects.get(name=service_name_deb)
+        except AptReleaseSigningService.DoesNotExist:
+            print(f"Registering Signing service {service_name_deb}")
+            AptReleaseSigningService.objects.create(
+                name=service_name_deb,
+                public_key=pub_key,
+                pubkey_fingerprint=fingerprint,
+                script=script_path,
+            )
+        else:
+            print(f"Signing service {service_name_deb} already present")
