c32beb065ad4f5a44032c9371ad3ed165aa74a34
Joshua Schmid <jaiks@posteo.de>
2021-11-04 11:00:43 +0100

    fix(pulp-woker): allow to have a GPG key generated in test mode
    Signed-off-by: Joshua Schmid <jaiks@posteo.de>

--- pulp-worker/entrypoint.sh
+++ pulp-worker/entrypoint.sh
@@ -6,15 +6,35 @@ if [[ -r "$GPG_KEY" ]]; then
     echo "[INFO] Enabling Signing API"
     # Import private key
     gpg --import $GPG_KEY
-    # Export public key
-    gpg --export -a > /tmp/public.key
-    # Export key fingerprint
-    gpg --with-fingerprint --with-colons /tmp/public.key 2>/dev/null | grep fpr: | head -n1 | cut -d: -f10 > /tmp/public.fpr
+elif [[ ! -r "${GPG_KEY}" && "${PULP_TEST}" -eq 1 ]]; then
+    echo "Could not find GPG key. Generating new"
+    gpg --batch --no-tty --pinentry-mode loopback --gen-key <<EOF
+Key-Type: 1
+Key-Length: 2048
+Subkey-Type: 1
+Subkey-Length: 2048
+Name-Real: Tester
+%no-protection
+Name-Email: test@test.com
+Expire-Date: 0
+EOF
+else
 
-    echo -e "5\ny\n" | gpg --batch --pinentry-mode loopback --yes --no-tty --command-fd 0 --expert --edit-key "$(cat /tmp/public.fpr)" trust
+echo -e "Could not load GPG key. "
+echo -e "If you're running this locally pass PULP_TEST=1 to generate a dummy key"
+echo -e "Aborting.."
+exit 1
 
-    pulpcore-manager shell < /opt/pulp/lib/register-signing-api.py
 fi
+
+# Export public key
+gpg --export -a > /tmp/public.key
+# Export key fingerprint
+gpg --with-fingerprint --with-colons /tmp/public.key 2>/dev/null | grep fpr: | head -n1 | cut -d: -f10 > /tmp/public.fpr
+
+echo -e "5\ny\n" | gpg --batch --pinentry-mode loopback --yes --no-tty --command-fd 0 --expert --edit-key "$(cat /tmp/public.fpr)" trust
+
+pulpcore-manager shell < /opt/pulp/lib/register-signing-api.py
 
 REDIS_URL=${PULP_REDIS_URL:-"localhost:6379"}
 WORKER_NAME=${WORKER_NAME:-"worker@%h"}
