ff664f08ae4e14ff3a731ca4b3ffee0ab72a53c7
Joshua Schmid <jaiks@posteo.de>
2021-11-04 10:59:49 +0100

    fix(sign): align with DebSign* and allow to catch bare exceptions
    Signed-off-by: Joshua Schmid <jaiks@posteo.de>

--- pulp-core/signing.py
+++ pulp-core/signing.py
@@ -12,8 +12,6 @@ with open("/tmp/public.key") as key:
         service_name = f"{NAME}-{fingerprint}"
         service_name_deb = f"{NAME}-{fingerprint}-deb"
         try:
-            AsciiArmoredDetachedSigningService.objects.get(name=service_name)
-        except AsciiArmoredDetachedSigningService.DoesNotExist:
             print(f"Registering Signing service {service_name}")
             AsciiArmoredDetachedSigningService.objects.create(
                 name=service_name,
@@ -21,9 +19,8 @@ with open("/tmp/public.key") as key:
                 pubkey_fingerprint=fingerprint,
                 script=script_path,
             )
-        else:
-            print(f"Signing service {service_name} already present")
-
+        except Exception as e:
+            print(f"Cought exception while creating AsciiArmoredDetachedSigningService: {e}")
         try:
             print(f"Registering Signing service {service_name_deb}")
             AptReleaseSigningService.objects.create(
